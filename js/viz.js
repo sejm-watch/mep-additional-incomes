d6696d37feb3415c316d5c6fe0aed8990e748b3e
kuba@sejm_watch <sejm.watch@gmail.com>
2021-03-20 11:22:31 +0100

    init

--- js/viz.js
+++ js/viz.js
@@ -1,4 +1,4 @@
-d3.csv("/data/declarations.csv", function(data) {
+d3.csv("../data/declarations.csv", function(data) {
     for (var i = 0; i < data.length; i++) {
         console.log(data[i].deputy_name);
     }
