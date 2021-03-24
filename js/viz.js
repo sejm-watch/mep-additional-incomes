3b9efbe44402769f5ef9945cce441334c2028434
kuba@sejm_watch <sejm.watch@gmail.com>
2021-03-24 23:41:10 +0100

    init

--- js/viz.js
+++ js/viz.js
@@ -14,6 +14,7 @@ String.prototype.toProperCase = function () {
     return this.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
 };
 
+
 const pSBC=(p,c0,c1,l)=>{
 	let r,g,b,P,f,t,h,i=parseInt,m=Math.round,a=typeof(c1)=="string";
 	if(typeof(p)!="number"||p<-1||p>1||typeof(c0)!="string"||(c0[0]!='r'&&c0[0]!='#')||(c1&&!a))return null;
@@ -38,7 +39,6 @@ const pSBC=(p,c0,c1,l)=>{
 	if(h)return"rgb"+(f?"a(":"(")+r+","+g+","+b+(f?","+m(a*1000)/1000:"")+")";
 	else return"#"+(4294967296+r*16777216+g*65536+b*256+(f?m(a*255):0)).toString(16).slice(1,f?undefined:-2)
 }
-
 
 
 function wrap(text, width) {
@@ -71,6 +71,49 @@ const median = arr => {
     nums = [...arr].sort((a, b) => a - b);
   return arr.length % 2 !== 0 ? nums[mid] : (nums[mid - 1] + nums[mid]) / 2;
 };
+
+
+function calculate_category_income(toggle_status, variant_status){
+  var category_income = [];
+  organization_categories.forEach(function(o){
+    var org_deputies = deputies_income.filter(x => x[o + "_salary_max"] != 0 && x.remuneration_type != "previous_occupations");
+    if(variant_status!='all'){org_deputies = org_deputies.filter(x => x[toggle_status] == variant_status);}
+
+    var deputies_count = org_deputies.length;
+    var deputies_additionally_paid = org_deputies.filter(x => parseInt(x[o + "_salary_max"]) > 0).length;
+    var perc_deputies_additionally_paid = deputies_additionally_paid/deputies_count
+
+    var occupations_count = org_deputies.map(x => parseInt(x.occupations_count)).reduce((a, b) => a + b, 0);
+    var paid_occupations_count = org_deputies.map(x => parseInt(x.paid_occupations_count)).reduce((a, b) => a + b, 0);
+
+    var total_salary_min = org_deputies.map(x => parseInt(x[o + "_salary_min"])).reduce((a, b) => a + b, 0)
+    var total_salary_max = org_deputies.map(x => parseInt(x[o + "_salary_max"])).reduce((a, b) => a + b, 0)
+
+    var avg_salary_min = org_deputies.map(x => parseInt(x[o + "_salary_min"])).reduce((a, b) => a + b, 0)/deputies_count;
+    var avg_salary_max = org_deputies.map(x => parseInt(x[o + "_salary_max"])).reduce((a, b) => a + b, 0)/deputies_count;
+
+    var median_salary_min = median(org_deputies.filter(x => parseInt(x[o + "_salary_min"]) > 0).map(x => parseInt(x[o + "_salary_min"])))
+    var median_salary_max = median(org_deputies.filter(x => parseInt(x[o + "_salary_max"]) > 0).map(x => parseInt(x[o + "_salary_max"])))
+
+    category_income.push({
+      'organization_category': o,
+      'deputies_count': deputies_count,
+      'deputies_additionally_paid': deputies_additionally_paid,
+      'perc_deputies_additionally_paid': perc_deputies_additionally_paid,
+      'occupations_count': occupations_count,
+      'paid_occupations_count': paid_occupations_count,
+      'total_salary_min': total_salary_min,
+      'total_salary_max': total_salary_max,
+      'avg_salary_min': avg_salary_min,
+      'avg_salary_max': avg_salary_max,
+      'median_salary_min': median_salary_min,
+      'median_salary_max': median_salary_max
+    })
+  });
+  category_income = category_income.filter(x => parseInt(x["total_salary_max"]) > 0)
+  category_income = category_income.sort((a,b) => (a.total_salary_max < b.total_salary_max) ? 1 : ((b.total_salary_max < a.total_salary_max) ? -1 : 0))
+  return category_income
+}
 
 
 function draw_column_chart(chart_container_id, tooltip_container, data, x_source, y_source, tooltip_sources, width, height){
@@ -271,7 +314,6 @@ function draw_cleveland_chart(chart_container_id, tooltip_container, data, x1_so
 
 function draw_lollipop_chart(chart_container_id, tooltip_container, data, x_source, y_source, tooltip_sources, width, height){
   var chart_container = d3.select(chart_container_id)
-
   // X axis
   var x = d3.scaleBand()
     .range([ 0,width ])
@@ -281,15 +323,6 @@ function draw_lollipop_chart(chart_container_id, tooltip_container, data, x_sour
     .attr("transform", "translate(0," + height + ")")
     .call(d3.axisBottom(x))
 
-  var tick_labels = chart_container.selectAll('.tick text')
-    .attr("font-size", "10px")
-  tick_labels.each(function(d, i){
-    words = d.split(" ")
-    words[0] = words[0].substring(0, 1) + "."
-    d3.select(this).text(words.join(" ").toProperCase())
-    if(i%2 !== 0) d3.select(this).attr("dy", 18);
-  });
-
   // Add Y axis
   var y = d3.scaleLinear()
     .domain([0, d3.max(data.map(function(d) {return d[y_source]}))*1.2])
@@ -298,6 +331,16 @@ function draw_lollipop_chart(chart_container_id, tooltip_container, data, x_sour
     .call(d3.axisLeft(y)
       .tickFormat(d3.format(".2s"))
     )
+
+  var tick_labels = chart_container.selectAll('.x_axis .tick text')
+    .attr("font-size", "10px")
+  tick_labels.each(function(d, i){
+    words = d.split(" ")
+    words[0] = words[0].substring(0, 1) + "."
+    d3.select(this).text(words.join(" ").toProperCase())
+    if(i%2 !== 0){d3.select(this).attr("dy", 18)}
+    else{d3.select(this).attr("dy", 7)}
+  });
 
   //lines
   var lines = chart_container.selectAll(".cleveland_lines")
@@ -321,18 +364,20 @@ function draw_lollipop_chart(chart_container_id, tooltip_container, data, x_sour
         .attr("y2", function(d) { return y(d[y_source]); })
 
   var r_photo = 40
+  chart_container.selectAll('.profile_photos').exit().remove()
   var defs = chart_container.append('svg:defs');
   data.forEach(function(d, i){
-      defs.append("svg:pattern")
-        .attr("id", "profile_photo" + i)
-        .attr("width", r_photo)
-        .attr("height", r_photo)
-        .append("svg:image")
-        .attr("xlink:href", d.dep_photo_url)
-        .attr("width", r_photo)
-        .attr("height", r_photo)
-        .attr("x", 0)
-        .attr("y", 0);
+    defs.append("svg:pattern")
+      .attr("class", "profile_photos")
+      .attr("id", "profile_photo" + d.dep_photo_url.split("/").slice(-1)[0].split(".")[0])
+      .attr("width", r_photo)
+      .attr("height", r_photo)
+      .append("svg:image")
+      .attr("xlink:href", d.dep_photo_url)
+      .attr("width", r_photo)
+      .attr("height", r_photo)
+      .attr("x", 0)
+      .attr("y", 0);
   })
 
 
@@ -360,7 +405,7 @@ function draw_lollipop_chart(chart_container_id, tooltip_container, data, x_sour
       .attr("class", "circles")
       .attr("cx", function(d) { return x(d[x_source]); })
       .attr("cy", function(d) { return y(d[y_source]); })
-      .style("fill", function(d, i){return "url(#profile_photo" + i + ")"})
+      .style("fill", function(d, i){return "url(#profile_photo" + d.dep_photo_url.split("/").slice(-1)[0].split(".")[0] + ")"})
       .attr("r", 0)
       .transition().delay(750).duration(250)
       .attr("r", r_photo/2)
@@ -379,9 +424,16 @@ function draw_horizontal_column_chart(chart_container_id, tooltip_container, dat
     .attr("transform", "translate(0," + height + ")")
     .call(d3.axisBottom(x))
 
-  chart_container.selectAll('.tick text').each(function(_,i){
-    if(i%2 !== 0) d3.select(this).attr("dy", 18);
+  var tick_labels = chart_container.selectAll('.x_axis .tick text')
+    .attr("font-size", "10px")
+  tick_labels.each(function(d, i){
+    if(i%2 !== 0){d3.select(this).attr("dy", 18)}
+    else{d3.select(this).attr("dy", 7)}
   });
+
+  //chart_container.selectAll('.x_axis .tick text').each(function(d, i){
+  //  if(i%2 !== 0) d3.select(this).attr("dy", 18);
+  //});
 
   // Add Y axis
   var y = d3.scaleLinear()
@@ -404,7 +456,7 @@ function draw_horizontal_column_chart(chart_container_id, tooltip_container, dat
       tooltip_container.transition()
         .duration(200)
         .style("opacity", .9);
-      tooltip_container.html("<b>" + d[x_source] + "</b><br/>from <b>" + (isNaN(d[tooltip_sources[0]]) ? '' : Math.round(d[tooltip_sources[0]]/100)/10) + "</b> up to <b>" + (isNaN(d[tooltip_sources[1]]) ? '' : Math.round(d[tooltip_sources[1]]/100)/10) + " kEUR</b><br/>paid to " + d.deputies_additionally_paid + " deputies")
+      tooltip_container.html("<b>" + d[x_source] + "</b><br/>from <b>" + (isNaN(d[tooltip_sources[0]]) ? '' : Math.round(d[tooltip_sources[0]]/100)/10) + "</b> up to <b>" + (isNaN(d[tooltip_sources[1]]) ? '' : Math.round(d[tooltip_sources[1]]/100)/10) + " kEUR</b><br/>" + d.deputies_additionally_paid + " deputies")
         .style("left", (event.pageX) + "px")
         .style("top", (event.pageY - 28) + "px");
       })
@@ -447,66 +499,77 @@ function switch_column_chart_colors(chart_container_id, y_source){
 
   var bars = chart_container.selectAll("rect")
   bars.each(function(d){
-    if(variant_status=='all'){
-      d3.select(this).transition().duration(750).attr("fill", Object.keys(party_colors).includes(d[y_source]) ? party_colors[d[y_source]]['fill'] : "#003399")
-    }
-    else if(d[y_source]!=variant_status){
-      d3.select(this).transition().duration(750).attr("fill", pSBC(0.8,Object.keys(party_colors).includes(d[y_source]) ? party_colors[d[y_source]]['fill'] : "#003399",false,true))
-    }
-    else{
-      d3.select(this).transition().duration(750).attr("fill", Object.keys(party_colors).includes(d[y_source]) ? party_colors[d[y_source]]['fill'] : "#003399")
-    }
+    if(variant_status=='all'){d3.select(this).transition().duration(1000).attr("fill", Object.keys(party_colors).includes(d[y_source]) ? party_colors[d[y_source]]['fill'] : "#003399")}
+    else if(d[y_source]!=variant_status){d3.select(this).transition().duration(1000).attr("fill", pSBC(0.8,Object.keys(party_colors).includes(d[y_source]) ? party_colors[d[y_source]]['fill'] : "#003399",false,true))}
+    else{d3.select(this).transition().duration(1000).attr("fill", Object.keys(party_colors).includes(d[y_source]) ? party_colors[d[y_source]]['fill'] : "#003399")}
   })
 
   var bar_labels = chart_container.selectAll(".bar_labels")
   bar_labels.each(function(d){
-    if(variant_status=='all'){
-      d3.select(this).transition().duration(750).attr("fill", Object.keys(party_colors).includes(d[y_source]) ? party_colors[d[y_source]]['text'] : "#FFFFFF")
-    }
-    else if(d[y_source]!=variant_status){
-      d3.select(this).transition().duration(750).attr("fill", "#FFFFFF")
-    }
-    else{
-      d3.select(this).transition().duration(750).attr("fill", Object.keys(party_colors).includes(d[y_source]) ? party_colors[d[y_source]]['text'] : "#FFFFFF")
-    }
+    if(variant_status=='all'){d3.select(this).transition().duration(1000).attr("fill", Object.keys(party_colors).includes(d[y_source]) ? party_colors[d[y_source]]['text'] : "#FFFFFF")}
+    else if(d[y_source]!=variant_status){d3.select(this).transition().duration(1000).attr("fill", "#FFFFFF")}
+    else{d3.select(this).transition().duration(1000).attr("fill", Object.keys(party_colors).includes(d[y_source]) ? party_colors[d[y_source]]['text'] : "#FFFFFF")}
   })
-
-  //tutaj zmienic politykow i kateogrie
 }
 
 
 
-function switch_cleveland_chart_colors(chart_container_id){
+function switch_cleveland_chart_colors(chart_container_id, y_source){
   var chart_container = d3.select(chart_container_id)
 
-  var bars = chart_container.selectAll("rect")
-  bars.each(function(d){
-    if(d.country!=variant_status){
-      d3.select(this).transition().duration(750).attr("fill", pSBC(0.8,"#003399",false,true))
-    }
-    else{
-      d3.select(this).transition().duration(750).attr("fill", "#003399")
-    }
+  var cleveland_lines = chart_container.selectAll(".cleveland_lines")
+  cleveland_lines.each(function(d){
+    if(variant_status=='all'){d3.select(this).transition().duration(1000).attr("stroke", "#003399")}
+    else if(d[y_source]!=variant_status){d3.select(this).transition().duration(1000).attr("stroke", pSBC(0.8,"#003399",false,true))}
+    else{d3.select(this).transition().duration(1000).attr("stroke", "#003399")}
   })
 
-  //tutaj zmienic politykow i kateogrie
+  var circles1 = chart_container.selectAll(".circles1")
+  circles1.each(function(d){
+    if(variant_status=='all'){d3.select(this).transition().duration(1000).style("fill", "#FFCC00").style("stroke", "#003399")}
+    else if(d[y_source]!=variant_status){d3.select(this).transition().duration(1000).style("fill", pSBC(0.8,"#FFCC00",false,true)).style("stroke", pSBC(0.8,"#003399",false,true))}
+    else{d3.select(this).transition().duration(1000).style("fill", "#FFCC00").style("stroke", "#003399")}
+  })
+
+  var circles2 = chart_container.selectAll(".circles2")
+  circles2.each(function(d){
+    if(variant_status=='all'){d3.select(this).transition().duration(1000).style("fill", "#6699ff").style("stroke", "#003399")}
+    else if(d[y_source]!=variant_status){d3.select(this).transition().duration(1000).style("fill", pSBC(0.8,"#6699ff",false,true)).style("stroke", pSBC(0.8,"#003399",false,true))}
+    else{d3.select(this).transition().duration(1000).style("fill", "#6699ff").style("stroke", "#003399")}
+  })
 }
 
 
 
 var toggle_status = 'country'
 function toggle_country_party_charts(){
+  if(variant_status!='all'){
+    top_n_deputies = deputies_income.filter((d,idx) => idx < 10 && d['salary_max']!=0)
+
+    category_income = calculate_category_income(toggle_status, 'all')
+    top_n_categories = category_income.filter((d) => d.organization_category != 'Unknown')
+    top_n_categories = top_n_categories.filter((d,idx) => idx < 10)
+    top_n_categories.push(top_n_categories.splice(top_n_categories.findIndex(function(e){return e.organization_category=='Other'}), 1)[0]);
+
+    d3.select("#top_right_section_heading").transition().duration(1000).text("TOP10 MEPs additional monthly income")
+    d3.select("#bottom_right_section_heading").transition().duration(1000).text("TOP10 sources of income - business areas")
+
+    draw_lollipop_chart("#top_right_chart", div_tooltip, top_n_deputies, 'deputy_name', 'salary_max', ['salary_min', 'salary_max'], top_right_width, top_right_height)
+    draw_horizontal_column_chart("#bottom_right_chart", div_tooltip, top_n_categories, 'organization_category', 'total_salary_max', ['total_salary_min', 'total_salary_max'], top_right_width, top_right_height)
+  }
+
   variant_status = 'all'
+
   var toggle = document.getElementById("toggle-button");
   if (toggle.checked && toggle_status=='country'){
-    toggle_status = 'party'
+    toggle_status = 'eu_party'
     d3.select("#label_by_party").transition().duration(300).style("fill", "#000000")
     d3.select("#label_by_country").transition().duration(300).style("fill", "#AAAAAA")
 
     draw_column_chart("#top_left_chart", div_tooltip, party_income, 'perc_deputies_additionally_paid', 'eu_party', ['deputies_additionally_paid','deputies_count'], top_left_width, top_left_height)
     draw_cleveland_chart("#top_middle_chart", div_tooltip, party_income, 'median_salary_min', 'median_salary_max', 'eu_party', ['median_salary_min', 'median_salary_max'], top_middle_width, top_middle_height)
   }
-  else if (!toggle.checked && toggle_status=='party') {
+  else if (!toggle.checked && toggle_status=='eu_party') {
     toggle_status = 'country'
     d3.select("#label_by_party").transition().duration(300).style("fill", "#AAAAAA")
     d3.select("#label_by_country").transition().duration(300).style("fill", "#000000")
@@ -520,15 +583,32 @@ function toggle_country_party_charts(){
 
 var variant_status = 'all'
 function switch_variant(d){
-  if(toggle_status=='country'){
-    if(variant_status==d.country){variant_status = 'all';}
-    else{variant_status = d.country;}
-    switch_column_chart_colors("#top_left_chart", 'country')
-    //draw_cleveland_chart("#top_middle_chart", div_tooltip, party_income, 'median_salary_min', 'median_salary_max', 'eu_party', ['median_salary_min', 'median_salary_max'], top_middle_width, top_middle_height)
+  if(variant_status==d[toggle_status]){
+    variant_status = 'all';
+    top_n_deputies = deputies_income.filter((d,idx) => idx < 10 && d['salary_max']!=0)
   }
-  else if(toggle_status=='party'){
-    if(variant_status==d.eu_party){variant_status = 'all';}
-    else{variant_status = d.eu_party;}
-    switch_column_chart_colors("#top_left_chart", 'eu_party')
+  else{
+    variant_status = d[toggle_status];
+    top_n_deputies = deputies_income.filter((d,idx) => d[toggle_status]==variant_status && d['salary_max']!=0);
+    top_n_deputies = top_n_deputies.filter((d,idx) => idx < 10);
   }
+
+  category_income = calculate_category_income(toggle_status, variant_status)
+  top_n_categories = category_income.filter((d) => d.organization_category != 'Unknown')
+  top_n_categories = top_n_categories.filter((d,idx) => idx < 10)
+  top_n_categories.push(top_n_categories.splice(top_n_categories.findIndex(function(e){return e.organization_category=='Other'}), 1)[0]);
+
+  if (variant_status!='all'){
+    d3.select("#top_right_section_heading").transition().duration(1000).text("TOP10 MEPs additional monthly income | " + variant_status)
+    d3.select("#bottom_right_section_heading").transition().duration(1000).text("TOP10 sources of income - business areas | " + variant_status)
+  }
+  else{
+    d3.select("#top_right_section_heading").transition().duration(1000).text("TOP10 MEPs additional monthly income")
+    d3.select("#bottom_right_section_heading").transition().duration(1000).text("TOP10 sources of income - business areas")
+  }
+
+  switch_column_chart_colors("#top_left_chart", toggle_status)
+  switch_cleveland_chart_colors("#top_middle_chart", toggle_status)
+  draw_lollipop_chart("#top_right_chart", div_tooltip, top_n_deputies, 'deputy_name', 'salary_max', ['salary_min', 'salary_max'], top_right_width, top_right_height)
+  draw_horizontal_column_chart("#bottom_right_chart", div_tooltip, top_n_categories, 'organization_category', 'total_salary_max', ['total_salary_min', 'total_salary_max'], top_right_width, top_right_height)
 }
